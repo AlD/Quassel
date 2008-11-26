ce143e90e4d6b113068865012b050cdc2e272a1c
Daniel Albers <daniel@lbers.com>
2008-11-26 21:00:02 +0100

    Style /quote as RawMsg

--- src/core/userinputhandler.cpp
+++ src/core/userinputhandler.cpp
@@ -331,6 +331,7 @@ void UserInputHandler::issueQuit(const QString &reason) {
 
 void UserInputHandler::handleQuote(const BufferInfo &bufferInfo, const QString &msg) {
   Q_UNUSED(bufferInfo)
+  emit displayMsg(Message::Raw, BufferInfo::StatusBuffer, "", msg);
   emit putRawLine(serverEncode(msg));
 }
 
