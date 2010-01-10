ba984fe05efa7e22c9f9b60f18d29f4d3e685a3e
Daniel Albers <daniel@lbers.com>
2010-01-10 16:04:39 +0100

    add support for Translate Toolkit to update.sh
    Falls back to Qt's lconvert if Translate Toolkit is unavailable.
    Duplicates will get weed out by msguniq either way.

--- po/update.sh
+++ po/update.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 if [ ! $# -eq 1 ]; then
   exec >&2
   echo "Usage: $0 <language>"
@@ -6,5 +6,18 @@ if [ ! $# -eq 1 ]; then
   exit 1
 fi
 
-msgmerge --no-wrap --width 1 -U $1 quassel.pot
+TS2PO=`type -p ts2po`
+LCONV=lconvert
+POT=quassel.pot
+BASE=$1
+PO=$BASE.po
+TS=$BASE.ts
+
+( [ -f $PO ] || ( [ -f $POT ] && cp $POT $PO ) )                      &&
+  $LCONV -i $PO -o $TS                                                &&
+  lupdate -no-obsolete ../src -ts $TS                                 &&
+  ${TS2PO:-$LCONV} -i $TS -o tmp.${PO} ${TS2PO:+--duplicates=msgctxt} &&
+  msguniq --use-first tmp.${PO} > ${PO}                               &&
+  rm $TS tmp.${PO}
+
 [ $? -ne 0 ] && echo "Something went wrong"
