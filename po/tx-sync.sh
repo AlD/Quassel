636c76ef0c57f0a2393b60ccebcd370a816e59c1
Daniel Albers <daniel@lbe.rs>
2012-01-31 20:57:58 +0100

    new file:   tx-sync.sh

---
+++ po/tx-sync.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+pushd /usr/src/quassel && ( 
+  currb=$(git name-rev --name-only HEAD)
+  git checkout -q i18n-tx-sync && (
+    VISUAL=/bin/true /usr/src/quassel/po/pull-from-transifex.sh -f &&
+      git push -q
+  ); git checkout -q "$currb"
+); popd
