472e37ebc703095f3f174d7f708da0749ca2d251
Daniel Albers <daniel@lbe.rs>
2018-10-15 02:06:23 +0200

    Unread backlog fetcher: Only fetch actually unread messages
    PerBufferUnreadBacklogRequester hands a lastSeenMsgId to the
    select_messagesNewerThan query, which fetches messageIds >= lastSeenMsgId
    (bounded by buffer.lastmsgid). This causes every buffer active in the client to
    return at least 1 message, no matter how old it is.
    
    Change the query so that messageIds have to be strictly larger than
    lastSeenMsgId.

--- src/core/SQL/PostgreSQL/select_messagesNewerThan.sql
+++ src/core/SQL/PostgreSQL/select_messagesNewerThan.sql
@@ -1,7 +1,7 @@
 SELECT messageid, time,  type, flags, sender, senderprefixes, realname, avatarurl, message
 FROM backlog
 JOIN sender ON backlog.senderid = sender.senderid
-WHERE backlog.messageid >= $1
+WHERE backlog.messageid > $1
 AND backlog.messageid <= (SELECT buffer.lastmsgid FROM buffer WHERE buffer.bufferid = $2)
 AND bufferid = $2
 ORDER BY messageid DESC
